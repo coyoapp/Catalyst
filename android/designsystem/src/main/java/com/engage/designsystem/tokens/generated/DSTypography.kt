6e5edf32f2900ca34b46cd755e5e391e2b98bae1
efehaiilo <105373721+efehaiilo@users.noreply.github.com>
2025-09-26 13:32:00 +0000

    CI: update tokens

---
+++ android/designsystem/src/main/java/com/engage/designsystem/tokens/generated/DSTypography.kt
@@ -0,0 +1,91 @@
+//
+// DSTypography.kt
+//
+// Do not edit directly, this file is generated from design tokens
+//
+
+package com.engage.designsystem.tokens.generated
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+public object DSTypography {
+    public val typography_h1 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(700),
+        fontSize = 32.sp,
+        lineHeight = 40.sp
+    )
+
+    public val typography_h2 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(600),
+        fontSize = 28.sp,
+        lineHeight = 36.sp
+    )
+
+    public val typography_h3 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(600),
+        fontSize = 24.sp,
+        lineHeight = 32.sp
+    )
+
+    public val typography_h4 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(600),
+        fontSize = 20.sp,
+        lineHeight = 28.sp
+    )
+
+    public val typography_body1 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(400),
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+
+    public val typography_body2 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(400),
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    )
+
+    public val typography_s1 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(600),
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+
+    public val typography_s2 = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(600),
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    )
+
+    public val typography_button = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(600),
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    )
+
+    public val typography_caption = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(400),
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    )
+
+    public val typography_overline = TextStyle(
+        fontFamily = DSFontFamily.lato,
+        fontWeight = FontWeight(400),
+        fontSize = 10.sp,
+        lineHeight = 16.sp
+    )
+
+}
---
+++ iOS/DesignSystem/Sources/DesignSystemKit/Tokens/Generated/DSTypography.swift
@@ -0,0 +1,21 @@
+//
+// DSTypography.swift
+//
+// Do not edit directly, this file is generated from design tokens
+//
+
+import SwiftUI
+
+public enum DSTypography {
+    public static let typographyH1 = Font.custom(""Lato"-Regular", size: 32.00)
+    public static let typographyH2 = Font.custom(""Lato"-Regular", size: 28.00)
+    public static let typographyH3 = Font.custom(""Lato"-Regular", size: 24.00)
+    public static let typographyH4 = Font.custom(""Lato"-Regular", size: 20.00)
+    public static let typographyBody1 = Font.custom(""Lato"-Regular", size: 16.00)
+    public static let typographyBody2 = Font.custom(""Lato"-Regular", size: 14.00)
+    public static let typographyS1 = Font.custom(""Lato"-Regular", size: 16.00)
+    public static let typographyS2 = Font.custom(""Lato"-Regular", size: 14.00)
+    public static let typographyButton = Font.custom(""Lato"-Regular", size: 14.00)
+    public static let typographyCaption = Font.custom(""Lato"-Regular", size: 12.00)
+    public static let typographyOverline = Font.custom(""Lato"-Regular", size: 10.00)
+}
