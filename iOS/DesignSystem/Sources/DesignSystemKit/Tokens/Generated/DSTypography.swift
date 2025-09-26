2459a74bb0a6116f5d766a88d0c37ab663f37154
efehaiilo <105373721+efehaiilo@users.noreply.github.com>
2025-09-26 14:19:06 +0000

    CI: update tokens

--- iOS/DesignSystem/Sources/DesignSystemKit/Tokens/Generated/DSTypography.swift
+++ iOS/DesignSystem/Sources/DesignSystemKit/Tokens/Generated/DSTypography.swift
@@ -7,15 +7,15 @@
 import SwiftUI
 
 public enum DSTypography {
-    public static let H1 = Font.custom("Lato-Regular", size: 32.00)
-    public static let H2 = Font.custom("Lato-Regular", size: 28.00)
-    public static let H3 = Font.custom("Lato-Regular", size: 24.00)
-    public static let H4 = Font.custom("Lato-Regular", size: 20.00)
-    public static let BODY1 = Font.custom("Lato-Regular", size: 16.00)
-    public static let BODY2 = Font.custom("Lato-Regular", size: 14.00)
-    public static let S1 = Font.custom("Lato-Regular", size: 16.00)
-    public static let S2 = Font.custom("Lato-Regular", size: 14.00)
-    public static let BUTTON = Font.custom("Lato-Regular", size: 14.00)
-    public static let CAPTION = Font.custom("Lato-Regular", size: 12.00)
-    public static let OVERLINE = Font.custom("Lato-Regular", size: 10.00)
+    public static let h1 = Font.custom("Lato-Regular", size: 32.00)
+    public static let h2 = Font.custom("Lato-Regular", size: 28.00)
+    public static let h3 = Font.custom("Lato-Regular", size: 24.00)
+    public static let h4 = Font.custom("Lato-Regular", size: 20.00)
+    public static let body1 = Font.custom("Lato-Regular", size: 16.00)
+    public static let body2 = Font.custom("Lato-Regular", size: 14.00)
+    public static let s1 = Font.custom("Lato-Regular", size: 16.00)
+    public static let s2 = Font.custom("Lato-Regular", size: 14.00)
+    public static let button = Font.custom("Lato-Regular", size: 14.00)
+    public static let caption = Font.custom("Lato-Regular", size: 12.00)
+    public static let overline = Font.custom("Lato-Regular", size: 10.00)
 }
