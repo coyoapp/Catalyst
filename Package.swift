9c4a5e48cc534415f5edb70dc56df4595ed3da82
Efe Durmaz <durmazefe.ed@gmail.com>
2025-11-14 12:05:53 +0100

    back to initial state fo external spm

--- Package.swift
+++ Package.swift
@@ -25,10 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DesignSystemKit",
-            path: "iOS/DesignSystem/Sources/DesignSystemKit",
-            resources: [
-                .process("Resources")
-            ]
+            path: "iOS/DesignSystem/Sources/DesignSystemKit"
         )
     ]
 )
