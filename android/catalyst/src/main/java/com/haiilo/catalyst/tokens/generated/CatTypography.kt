beae10ff46b1ce2932a5267871e57e63175cc49c
efehaiilo <105373721+efehaiilo@users.noreply.github.com>
2026-02-03 10:53:14 +0000

    CI: update tokens

--- android/catalyst/src/main/java/com/haiilo/catalyst/tokens/generated/CatTypography.kt
+++ android/catalyst/src/main/java/com/haiilo/catalyst/tokens/generated/CatTypography.kt
@@ -18,75 +18,64 @@ public object CatTypography {
         fontSize = 32.sp,
         lineHeight = 40.sp
     )
-
     public val h2 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(600),
         fontSize = 28.sp,
         lineHeight = 36.sp
     )
-
     public val h3 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(600),
         fontSize = 24.sp,
         lineHeight = 32.sp
     )
-
     public val h4 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(600),
         fontSize = 20.sp,
         lineHeight = 28.sp
     )
-
     public val body1 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(400),
         fontSize = 16.sp,
         lineHeight = 24.sp
     )
-
     public val body2 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(400),
         fontSize = 14.sp,
         lineHeight = 20.sp
     )
-
     public val s1 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(600),
         fontSize = 16.sp,
         lineHeight = 24.sp
     )
-
     public val s2 = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(600),
         fontSize = 14.sp,
         lineHeight = 20.sp
     )
-
     public val button = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(600),
         fontSize = 14.sp,
         lineHeight = 20.sp
     )
-
     public val caption = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(400),
         fontSize = 12.sp,
         lineHeight = 16.sp
     )
-
     public val overline = TextStyle(
         fontFamily = CatFontFamily.lato,
         fontWeight = FontWeight(400),
         fontSize = 10.sp,
         lineHeight = 16.sp
     )
-
 }
