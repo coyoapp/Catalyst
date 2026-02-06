7e52a1d5d37133a7c34d498341be980ea03802d6
Efe Durmaz <105373721+efehaiilo@users.noreply.github.com>
2026-02-06 14:34:19 +0100

    build(spm): increase stability in iOS integration (#19)
    Co-authored-by: Efe Durmaz <durmazefe.ed@gmail.com>

--- Package.swift
+++ Package.swift
@@ -25,7 +25,10 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Catalyst",
-            path: "iOS/Catalyst/Sources/Catalyst"
+            path: "iOS/Catalyst/Sources/Catalyst",
+            resources: [
+                .process("Resources")
+            ]
         )
     ]
 )
