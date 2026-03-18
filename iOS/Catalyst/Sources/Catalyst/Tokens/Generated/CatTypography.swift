6faed4b2bb77240ce9925b14071a443051505ba2
AminHeidaridahoee <amin.heidaridahoee@haiilo.com>
2026-03-18 11:13:13 +0100

    feat(tokens): typographty support UIKit fonts

--- iOS/Catalyst/Sources/Catalyst/Tokens/Generated/CatTypography.swift
+++ iOS/Catalyst/Sources/Catalyst/Tokens/Generated/CatTypography.swift
@@ -5,6 +5,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 public enum CatTypography {
     public static let h1 = Font.custom("Lato-Bold", size: 32.00)
@@ -19,4 +20,19 @@ public enum CatTypography {
     public static let button2 = Font.custom("Lato-Regular", size: 16.00)
     public static let caption = Font.custom("Lato-Regular", size: 12.00)
     public static let overline = Font.custom("Lato-Regular", size: 10.00)
+}
+
+public enum CatTypographyUIFont {
+    public static let h1 = UIFont(name: "Lato-Bold", size: 32)!
+    public static let h2 = UIFont(name: "Lato-Semibold", size: 28)!
+    public static let h3 = UIFont(name: "Lato-Semibold", size: 24)!
+    public static let h4 = UIFont(name: "Lato-Semibold", size: 20)!
+    public static let body1 = UIFont(name: "Lato-Regular", size: 16)!
+    public static let body2 = UIFont(name: "Lato-Regular", size: 14)!
+    public static let s1 = UIFont(name: "Lato-Semibold", size: 16)!
+    public static let s2 = UIFont(name: "Lato-Semibold", size: 14)!
+    public static let button1 = UIFont(name: "Lato-Semibold", size: 16)!
+    public static let button2 = UIFont(name: "Lato-Regular", size: 16)!
+    public static let caption = UIFont(name: "Lato-Regular", size: 12)!
+    public static let overline = UIFont(name: "Lato-Regular", size: 10)!
 }
