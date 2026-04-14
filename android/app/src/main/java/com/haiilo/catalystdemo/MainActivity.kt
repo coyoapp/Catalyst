ebf7816f71ca82700b1be9d43457b901a88beaef
Efe Durmaz <durmazefe.ed@gmail.com>
2026-04-14 09:17:00 +0200

    fix(linting): Removed unused imports

--- android/app/src/main/java/com/haiilo/catalystdemo/MainActivity.kt
+++ android/app/src/main/java/com/haiilo/catalystdemo/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,7 +19,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.haiilo.catalyst.R
